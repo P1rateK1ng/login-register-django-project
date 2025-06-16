6ba03786f04d272b611ad18de62c898ee0d94248
p1rate_k1ng <azamatik4ik@gmail.com>
2025-06-16 13:25:09 +0600

    moved the templates from base diriectory, to the directory in 'accounts' app

--- config/settings.py
+++ config/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'accounts' / 'templates' / 'accounts' ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
