cabac67913e9e30010daf935b75d54f7c340e342
p1rate_k1ng <azamatik4ik@gmail.com>
2025-06-16 13:15:07 +0600

    added requirements.txt

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('captcha/', include('captcha.urls')),
+    # path('captcha/', include('captcha.urls')),
     path('', include('accounts.urls')),
 ]
